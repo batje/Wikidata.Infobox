4108899edd844447b388db88515a4cd55980a462
Reinier Battenberg <reinier@mountbatten.nl>
2016-12-23 15:26:20 +0100

    Added code to build documentation

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,76 @@
 import babel from 'rollup-plugin-babel';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import bowerResolve from 'rollup-plugin-bower-resolve';
+import string from 'rollup-plugin-string';
+import filesize from 'rollup-plugin-filesize';
 
 export default {
+  banner: `/**
+   * Copyright (c) 2016, Reinier Battenberg
+   * All rights reserved.
+   *
+   * Source code can be found at:
+   * https://github.com/batje/Wikidata.Infobox
+   *
+   * @license GPL 3.0
+   */
+`,
+  footer: "/* You really read all code until the end? Good for you because there is some more here. I can not believe it, but I want to initiate my library automagically and the only way I found you can do that, is by adding it here. And oh and behold, that works. so:*/ \
+  console.log(\"Do this\"); \
+  var infobox = new Wikidata.InfoBox(); \
+  infobox.Monitor(); \
+    ",
   entry: 'src/index.js',
   format: 'umd',
-  sourceMap: false,
+  sourceMap: true,
   plugins: [
     nodeResolve({
       jsnext: true,
-      main: true
+      main: true,
+      preferBuiltins: false
+    }),
+    bowerResolve({
+      // if there's something your bundle requires that you DON'T
+      // want to include, add it to 'skip'
+      skip: [
+        'bower_components/wikidata-sdk/build/*',
+      ], // Default: []
+
+      // Override path to main file (relative to the module directory).
+      override: {
+        //  lodash: 'dist/lodash.js'
+      }
     }),
     commonjs({
       // non-CommonJS modules will be ignored, but you can also
       // specifically include/exclude files
-      //  include: 'node_modules/**', // Default: undefined
-      //    exclude: ['node_modules/foo/**', 'node_modules/bar/**'], // Default: undefined
+      include: [
+        'bower_components/js-yaml/dist/js-yaml.js',
+        //'bower_components/he/he.js',
+        //        'bower_components/handlebars/handlebars.amd.js',
+        'node_modules/q/q.js',
+        //        'bower_components/handlebars/node_modules/source-map/lib/source-map/*',
+        //        'bower_components/handlebars/node_modules/source-map/lib/source-map/source-map-consumer',
+        //        'bower_components/handlebars/node_modules/source-map/lib/source-map/source-node',
+        'node_modules/promised-handlebars/index.js',
+        'node_modules/buffer/index.js',
+        'node_modules/deep-aplus/index.js',
+        'node_modules/lodash-es/isArray.js',
+        'bower_components/spin.js/jquery.spin.js',
+        'bower_components/spin.js/spin.js',
+        // The following module makes rollup complain about fs not being available. But fs is not necessary
+        // for our project so we are cool.
+        'bower_components/es6-module-loader/dist/es6-module-loader-dev.js'
+      ], // Default: undefined
+      exclude: [
+        'bower_components/wikidata-sdk/build/*',
+        //,
+        //    'node_modules/handlebars/node_modules/source-map/lib/source-map/source-map-generator',
+        //    'node_modules/handlebars/node_modules/source-map/lib/source-map/source-map-consumer',
+        //    'node_modules/handlebars/node_modules/source-map/lib/source-map/source-node',
+      ], // Default: undefined
 
       // search for files other than .js files (must already
       // be transpiled by a previous plugin!)
@@ -25,19 +80,29 @@ export default {
       ignoreGlobal: false, // Default: false
 
       // if false then skip sourceMap generation for CommonJS modules
-      sourceMap: false, // Default: true
+      sourceMap: true, // Default: true
 
       // explicitly specify unresolvable named exports
       // (see below for more details)
       namedExports: {
-        './module.js': ['foo', 'bar']
-      } // Default: undefined
+        //      'node_modules/handlebars/dist/handlebars.min.js': ['handlebars']
+      }
     }),
+    sourcemaps(),
     babel({
-      exclude: "node_modules/**",
-      presets: ["es2015-rollup"]
+      exclude: ["node_modules/**", "bower_components/**"],
+      presets: ["es2015-rollup"],
+      runtimeHelpers: true
         // plugins appears to be ignored. use .babelrc
-    })
+    }),
+    string({
+      // Required to be specified
+      include: '../templates/**/*.hbs',
+
+      // Undefined by default
+      exclude: ['**/index.html']
+    }),
+    filesize()
   ],
   dest: 'dist/Wikidata.Infobox.js',
   moduleName: 'Wikidata'
